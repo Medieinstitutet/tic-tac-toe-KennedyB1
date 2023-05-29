b41e1540a9d21922157a4529ea9c5d0915043cec
KennedyB1 <1994_simon@live.se>
2023-05-29 15:57:39 +0200

    added ref

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@ import UserLogin from './components/UserLogin.vue';
 
 <template>
   <UserLogin />
+  <p></p>
 </template>
 
 <style scoped>
--- src/components/UserLogin.vue
+++ src/components/UserLogin.vue
@@ -1,34 +1,40 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 
-const player1Name = ref("");
-const player2Name = ref("");
+const names = ref<string[]>([]);
 
 const handlePlayer1Input = (e: Event) => {
   const inputElement = e.target as HTMLInputElement;
-  player1Name.value = capitalizeFirstLetter(inputElement.value);
+  const name = capitalizeFirstLetter(inputElement.value);
+  updateNames(0, name);
 }
 
 const handlePlayer2Input = (e: Event) => {
   const inputElement = e.target as HTMLInputElement;
-  player2Name.value = capitalizeFirstLetter(inputElement.value);
+  const name = capitalizeFirstLetter(inputElement.value);
+  updateNames(1, name);
 }
 
 const capitalizeFirstLetter = (name: string): string => {
   return name.charAt(0).toUpperCase() + name.slice(1);
 }
 
+const updateNames = (index: number, name: string) => {
+  names.value[index] = name;
+}
+
 const startGame = () => {
-  console.log("Player 1 Name:", player1Name.value);
-  console.log("Player 2 Name:", player2Name.value);
+  console.log("Player 1 Name:", names.value[0]);
+  console.log("Player 2 Name:", names.value[1]);
+  console.log(names.value)
 }
 </script>
 
 <template>
   <form @submit.prevent="startGame" class="centered-form">
-    <input type="text" id="player1" v-model="player1Name" @input="handlePlayer1Input" placeholder="Player 1 Name" required>
+    <input type="text" id="player1" :value="names[0]" @input="handlePlayer1Input" placeholder="Player 1 Name" required>
 
-    <input type="text" id="player2" v-model="player2Name" @input="handlePlayer2Input" placeholder="Player 2 Name" required>
+    <input type="text" id="player2" :value="names[1]" @input="handlePlayer2Input" placeholder="Player 2 Name" required>
 
     <button type="submit">Start Game</button>
   </form>
