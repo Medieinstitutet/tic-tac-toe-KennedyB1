884214b3949cff4713d8af5dc0f884ecc6c21221
KennedyB1 <1994_simon@live.se>
2023-05-29 15:02:03 +0200

    User login

--- src/components/UserLogin.vue
+++ src/components/UserLogin.vue
@@ -1,39 +1,50 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 
+const player1Name = ref("");
+const player2Name = ref("");
 
+const handlePlayer1Input = (e: Event) => {
+  const inputElement = e.target as HTMLInputElement;
+  player1Name.value = capitalizeFirstLetter(inputElement.value);
+}
 
-const usersInput = ref("");
+const handlePlayer2Input = (e: Event) => {
+  const inputElement = e.target as HTMLInputElement;
+  player2Name.value = capitalizeFirstLetter(inputElement.value);
+}
 
-const startGame = (e: Event) => {
+const capitalizeFirstLetter = (name: string): string => {
+  return name.charAt(0).toUpperCase() + name.slice(1);
+}
 
-    // https://medieinstitutet.sharepoint.com/sites/FED22D/Delade%20dokument/Forms/AllItems.aspx?FolderCTID=0x012000FCDBD6389286214B9B1AE945F6A1AAE7&sortField=Created&isAscending=true&id=%2Fsites%2FFED22D%2FDelade%20dokument%2FJavascript%20%2D%20F%C3%B6rdjupning%2FRecordings%2FJavascript%20%2D%20F%C3%B6rdjupning%20%2D%20Demon%20och%20fr%C3%A5gor%2D20230519%5F135012%2DM%C3%B6tesinspelning%2Emp4&viewid=cf5f9ac3%2Ddec3%2D43d4%2D8bb8%2D4f9a2473ae5d&parent=%2Fsites%2FFED22D%2FDelade%20dokument%2FJavascript%20%2D%20F%C3%B6rdjupning%2FRecordings
+const startGame = () => {
+  console.log("Player 1 Name:", player1Name.value);
+  console.log("Player 2 Name:", player2Name.value);
 }
 </script>
 
-
 <template>
-    <form @submit.prevent="startGame">
-        <label for="player1">Player 1 Name:</label>
-        <input type="text" id="player1" :value="usersInput" required>
+  <form @submit.prevent="startGame" class="centered-form">
+    <input type="text" id="player1" v-model="player1Name" @input="handlePlayer1Input" placeholder="Player 1 Name" required>
 
-        <label for="player2">Player 2 Name:</label>
-        <input type="text" id="player2" :value="usersInput" required>
-
-        <button type="submit">Start Game</button>
-    </form>
-</template>
-
-<!--<template>
-  <form @submit.prevent="startGame">
-    <label for="player1">Player 1 Name:</label>
-    <input type="text" id="player1" v-model="player1Name" required>
-
-    <label for="player2">Player 2 Name:</label>
-    <input type="text" id="player2" v-model="player2Name" required>
+    <input type="text" id="player2" v-model="player2Name" @input="handlePlayer2Input" placeholder="Player 2 Name" required>
 
     <button type="submit">Start Game</button>
   </form>
-</template>-->
+</template>
 
-  
+<style scoped>
+.centered-form {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 10px;
+}
+
+.centered-form input,
+.centered-form button {
+  margin: 5px;
+}
+</style>
